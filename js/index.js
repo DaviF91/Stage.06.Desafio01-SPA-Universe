b6293fad76bd77438cf634dba1ec007343f9a153
Davi Ferreira <daviferreiraaew@gmail.com>
2023-01-31 19:45:32 -0300

    Finalizing the features

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const routes = {
+  "/home": "/pages/home.html",
+  "/theUniverse": "/pages/universe.html",
+  "/exploration": "/pages/exploration.html",
+  "/404": "/pages/404.html"
+}
+function route(event) {
+  event = event || window.event
+  event.preventDefault()
+
+  window.history.pushState({}, "", event.target.href)
+
+  handle()
+}
+
+function handle(){
+  const { pathname } = window.location
+  const route = routes[pathname] || routes["/home"]
+
+  fetch(route)
+  .then(data=> data.text())
+  .then(html => {
+    document.querySelector('#app').innerHTML = html
+  })   
+}
+handle()
+window.onpopstate = ()=> handle()
+window.route = () => route()
