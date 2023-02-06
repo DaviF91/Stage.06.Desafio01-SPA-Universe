2ad9d9cd2418315639f422240974182fc735a98a
Davi Ferreira <daviferreiraaew@gmail.com>
2023-02-06 19:55:22 -0300

    Adicionando estilo e finalizando aplicação.

--- js/index.js
+++ js/index.js
@@ -11,3 +11,28 @@ router.handle()
 
 window.onpopstate = ()=> router.handle()
 window.route = () => router.route()
+
+
+//background
+const body = document.querySelector('body')
+const home = document.querySelector('#home')
+const universe = document.querySelector('#universe')
+const exploration = document.querySelector('#exploration')
+
+home.addEventListener('click', () => {
+  body.classList.add('homeBackground')
+  body.classList.remove('universeBackground')
+  body.classList.remove('explorationBackground')
+})
+
+universe.addEventListener('click', () => {
+  body.classList.remove('homeBackground')
+  body.classList.add('universeBackground')
+  body.classList.remove('explorationBackground')
+})
+
+exploration.addEventListener('click', () => {
+  body.classList.remove('homeBackground')
+  body.classList.remove('universeBackground')
+  body.classList.add('explorationBackground')
+})
